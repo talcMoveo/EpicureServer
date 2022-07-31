ee5e6e2218a97b760776efddb99f0bfceef77cb3
Tal Cohen <talc@moveo.co.il>
2022-07-31 11:01:14 +0300

    small changes plus routing change

--- handlers/searchHandler.js
+++ handlers/searchHandler.js
@@ -4,9 +4,19 @@ const restaurantModel = require("../schemes/restaurantScheme").RestaurantModel;
 
 const getSearchRes = async (searchWord) => {
     const regExStr = new RegExp(".*" + searchWord + ".*");
-    const chefRes = await chefModel.find({ name : regExStr });
-    const dishRes = await dishModel.find({ name : regExStr });
-    const restaurantRes = await restaurantModel.find({ name : regExStr });
+
+    const [chefRes, dishRes, restaurantRes] = await Promise.all(
+        [
+            chefModel.find({ name : regExStr }),
+            dishModel.find({ name : regExStr }),
+            restaurantModel.find({ name : regExStr })
+        ]
+        );
+        
+    // const chefRes = await chefModel.find({ name : regExStr });
+    // const dishRes = await dishModel.find({ name : regExStr });
+    // const restaurantRes = await restaurantModel.find({ name : regExStr });
+
     return ({
         chefResults : chefRes,
         dishResults : dishRes,
---
+++ routers/apiRouter.js
@@ -0,0 +1,7 @@
+const apiRouter = require("express").Router();
+
+const v1Router = require("./v1Router").v1Router;
+
+apiRouter.use("/v1", v1Router);
+
+module.exports = { apiRouter };
